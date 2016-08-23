32884388fe8b4dce21befb7ce4bb8afc5eefcee7
PetrovPetar <petrov90stz@gmail.com>
2016-08-23 23:19:40 +0300

    dd

--- WebApplication/Classes/Utils.cs
+++ WebApplication/Classes/Utils.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿
 namespace WebApplication.Classes
 {
@@ -15,22 +14,4 @@ namespace WebApplication.Classes
         }
 
     }
-=======
-﻿
-namespace WebApplication.Classes
-{
-    public class Utils
-    {
-        public static string CutText(string text, int maxLength = 450)
-        {
-            if (text == null || text.Length <= maxLength)
-            {
-                return text;
-            }
-            var shortText = text.Substring(0, maxLength - 3) + "...";
-            return shortText;
-        }
-
-    }
->>>>>>> d792e6801b2b6d3c9a4d9063835e002fabf139f1
 }
